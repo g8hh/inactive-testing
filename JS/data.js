c76a2e91c163c775806e3d13095dcc716f597570
MrBacon470 <Mrbacon470@outlook.com>
2022-09-03 15:50:25 -0500

    v0.0.3

--- JS/data.js
+++ JS/data.js
@@ -18,7 +18,7 @@ function getDefaultObject() {
         currentTab: 0,
         settingsToggles: [true,true],
         buyAmounts: [0,0],
-        currentUpdate: 'v0.0.2',
+        currentUpdate: 'v0.0.3',
         devSpeed: 1,
     }
 }
